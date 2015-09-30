ae18bc17d7ad6443e8a1c9965ca21bc50952ac12
John Cody Nemmer <jonemmer@hotmail.com>
2015-09-30 14:34:58 -0700

    I added if/else statements and error corrections at the same time

--- guessing_game2.js
+++ guessing_game2.js
@@ -1,15 +1,29 @@
 var question1 = prompt('Do I like to Downhill Mountain Bike?');
+
+if(question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y')  {
+	alert("Yes, I love to Downhill Mountain Bike!");
+} else {
+	alert("No, I do love to Downhill Mountain Bike!");
+}
+
 var question2 = prompt('Do I prefer 26 inch tires over 29 inch tires, yes or no?');
-var question3 = prompt('Why did you answer yes or no on the last question?')
-var question4 = prompt('Why do you think I would want 8 inchs of travel when I am riding downhill?');
+
+if(question2.toLowerCase() === 'yes' || question2.toLowerCase() === 'y' )  {
+  alert("That\'s correct, 26 inch tires give you better control!");	
+} else {
+	alert("No, I don\'t use 29 inch tires  due to less control.");
+}
+
+
+var question3 = prompt('Is 8 inches more preferred or 6 inchs of travel when downhill riding, yes or no?');
+
+if(question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y')  {
+	alert("Yes, 8 inches of travel absorbs more impact!");
+} else {
+	alert("No, 6 inches of travel doesn\'t absorb as much impact as 8 inches");
+}
 
 console.log('Question 1 response: ' + question1);
 console.log('Question 2 response: ' + question2);
 console.log('Question 3 response: ' + question3);
 
-alert('Do you like to downhill mountain bike? You said ' + question1 + '!');
-alert('Do I prefer 26 inch tires over 29 inch tires, you said ' 
-	   + question2 + '!' + ' Your reason was ' + '"' + question3 + 
-	   '"' + '!' );
-alert('Why do you think I would prefer 8 inches of travel, you said ' + '"' 
-	+ question4 + '"' + '!');
