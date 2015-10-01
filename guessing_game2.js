57f59e31c77ee9e86b7cbf93fdcf891034e399ed
Heidi Laursen <heidilaursen@yahoo.com>
2015-10-01 14:15:07 -0700

    Move logic into functions update

--- guessing_game2.js
+++ guessing_game2.js
@@ -1,31 +1,38 @@
 var tally = 0;
-var question1 = prompt('Do I like to Downhill Mountain Bike?');
 
-if(question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y')  {
-	alert("Yes, I love to Downhill Mountain Bike!");
-	tally++;
-} else {
-	alert("No, I do love to Downhill Mountain Bike!");
+function ques1() {
+	var question1 = prompt('Do I like to Downhill Mountain Bike?');
+
+	if(question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y')  {
+		alert("Yes, I love to Downhill Mountain Bike!");
+		tally++;
+	} else {
+		alert("No, I do love to Downhill Mountain Bike!");
+	}
 }
+ques1();
 
-var question2 = prompt('Do I prefer 26 inch tires over 29 inch tires, yes or no?');
-
-if(question2.toLowerCase() === 'yes' || question2.toLowerCase() === 'y' )  {
-  alert("That\'s correct, 26 inch tires give you better control!");	
-  tally++;
-} else {
-	alert("No, I don\'t use 29 inch tires  due to less control.");
+function ques2() {
+	var question2 = prompt('Do I prefer 26 inch tires over 29 inch tires, yes or no?');
+	if(question2.toLowerCase() === 'yes' || question2.toLowerCase() === 'y' )  {
+	  alert("That\'s correct, 26 inch tires give you better control!");
+	  tally++;
+	} else {
+		alert("No, I don\'t use 29 inch tires  due to less control.");
+	}
 }
+ques2();
 
-
-var question3 = prompt('Is 8 inches more preferred or 6 inchs of travel when downhill riding, yes or no?');
-
-if(question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y')  {
-	alert("Yes, 8 inches of travel absorbs more impact!");
-	tally++;
-} else {
-	alert("No, 6 inches of travel doesn\'t absorb as much impact as 8 inches");
+function ques3() {
+	var question3 = prompt('Is 8 inches more preferred or 6 inchs of travel when downhill riding, yes or no?');
+	if(question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y')  {
+		alert("Yes, 8 inches of travel absorbs more impact!");
+		tally++;
+	} else {
+		alert("No, 6 inches of travel doesn\'t absorb as much impact as 8 inches");
+	}
 }
+ques3();
 
 if(tally >= 2) {
 	alert("You\'d make a great mountain biker!");
@@ -38,4 +45,3 @@ alert("You got " + tally + " out of 3 Questions!" );
 console.log('Question 1 response: ' + question1);
 console.log('Question 2 response: ' + question2);
 console.log('Question 3 response: ' + question3);
-
