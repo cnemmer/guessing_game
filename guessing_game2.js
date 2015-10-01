4d657f7eae21d87aeecd78ef519415071be15599
Heidi Laursen <heidilaursen@yahoo.com>
2015-10-01 14:23:29 -0700

    Move alerts to DOM

--- guessing_game2.js
+++ guessing_game2.js
@@ -1,13 +1,17 @@
 var tally = 0;
 
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+
 function ques1() {
 	var question1 = prompt('Do I like to Downhill Mountain Bike?');
 
 	if(question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y')  {
-		alert("Yes, I love to Downhill Mountain Bike!");
+		one.innerHTML = "Yes, I love to Downhill Mountain Bike!";
 		tally++;
 	} else {
-		alert("No, I do love to Downhill Mountain Bike!");
+		one.innerHTML = "No, I do love to Downhill Mountain Bike!";
 	}
 }
 ques1();
@@ -15,10 +19,10 @@ ques1();
 function ques2() {
 	var question2 = prompt('Do I prefer 26 inch tires over 29 inch tires, yes or no?');
 	if(question2.toLowerCase() === 'yes' || question2.toLowerCase() === 'y' )  {
-	  alert("That\'s correct, 26 inch tires give you better control!");
+	  two.innerHTML = "That\'s correct, 26 inch tires give you better control!";
 	  tally++;
 	} else {
-		alert("No, I don\'t use 29 inch tires  due to less control.");
+		two.innerHTML = "No, I don\'t use 29 inch tires  due to less control.";
 	}
 }
 ques2();
@@ -26,10 +30,10 @@ ques2();
 function ques3() {
 	var question3 = prompt('Is 8 inches more preferred or 6 inchs of travel when downhill riding, yes or no?');
 	if(question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y')  {
-		alert("Yes, 8 inches of travel absorbs more impact!");
+		three.innerHTML = "Yes, 8 inches of travel absorbs more impact!";
 		tally++;
 	} else {
-		alert("No, 6 inches of travel doesn\'t absorb as much impact as 8 inches");
+		three.innerHTML = "No, 6 inches of travel doesn\'t absorb as much impact as 8 inches";
 	}
 }
 ques3();
