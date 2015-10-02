0cf5361ed8f966a42695e270428c3f006dd2cd23
Apple <apple@Apples-MacBook-Pro-3.local>
2015-10-02 13:43:16 -0700

    Added images with each correct response

--- guessing_game2.js
+++ guessing_game2.js
@@ -4,6 +4,10 @@ var one = document.getElementById('one');
 var two = document.getElementById('two');
 var three = document.getElementById('three');
 
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
+
 var questions = ['Do I like to Downhill Mountain Bike?', 
                  'Do I like to Downhill Mountain Bike?',
                  'Is 8 inches more preferred or 6 inchs of travel when downhill riding, yes or no?'];
@@ -14,7 +18,8 @@ function ques1() {
 
 	if(question1.toLowerCase() === answers[0] || question1.toLowerCase() === answers[1]) {
 		one.innerHTML = "Yes, I love to Downhill Mountain Bike!";
-		tally++;
+		img1.innerHTML = '<img src="img/img1.png" width = "200" />';
+		tally++;		
 	} else {
 		one.innerHTML = "No, I do love to Downhill Mountain Bike!";
 	}
@@ -25,6 +30,7 @@ function ques2() {
 	var question2 = prompt(questions[1]);
 	if(question2.toLowerCase() === answers[0] || question2.toLowerCase() === answers[1]) {
 	  two.innerHTML = "That\'s correct, 26 inch tires give you better control!";
+	  img2.innerHTML = '<img src="img/img2.png" width = "200" />';
 	  tally++;
 	} else {
 		two.innerHTML = "No, I don\'t use 29 inch tires  due to less control.";
@@ -36,6 +42,7 @@ function ques3() {
 	var question3 = prompt(questions[2]);
 	if(question3.toLowerCase() === answers[0] || question3.toLowerCase() === answers[1]) {
 		three.innerHTML = "Yes, 8 inches of travel absorbs more impact!";
+		img3.innerHTML = '<img src="img/img3.jpeg" width = "200" />';
 		tally++;
 	} else {
 		three.innerHTML = "No, 6 inches of travel doesn\'t absorb as much impact as 8 inches";
